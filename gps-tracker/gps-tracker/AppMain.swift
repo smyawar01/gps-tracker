7389e77f04000de2f6a1f6c2e3d3d69c8233f8fd
Yawar M <smyawar01@gmail.com>
2023-08-27 14:59:53 +0500

    - First commit with project files

---
+++ gps-tracker/gps-tracker/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  gps_trackerApp.swift
+//  gps-tracker
+//
+//  Created by muhammad Yawar on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
